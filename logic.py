f7a85e125859d82e2843e9d2dc054ec1f2a5fc05
prajyot birajdar <106489353+itsbilyatt@users.noreply.github.com>
2025-03-22 17:25:25 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -357,13 +357,13 @@ try:
             
             # try:
             #     # Attempt to read the file normally
-            df = pd.read_csv("Parts.csv", delimiter=";")
+            # df = pd.read_csv("Parts.csv", delimiter=";")
             # except UnicodeDecodeError as e:
             #     # Handle UnicodeDecodeError (skip or log it)
             #     print(f"Error reading {file}: {e}")
             # else:
             #     # Attempt to read the "Parts.csv" file if the first read fails
-            #     df = pd.read_csv(file)
+            df = pd.read_csv(file)
             #     print("Successfully read Parts.csv with ';' delimiter.")
             # except Exception as inner_e:
             #     # Handle any other error that might occur in reading "Parts.csv"
